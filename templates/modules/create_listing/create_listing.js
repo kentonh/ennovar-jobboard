34230b5a1846c975d3ef4cb0b4da6111cf0918e0
zrpearson36 <zachary@passfail.com>
2016-01-07 16:03:28 -0600

    job create/ job view pages (version .1)
    Little styling, using dummy data. Data entered on the create page is not
    saved.

---
+++ templates/modules/create_listing/create_listing.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient)
+{
+  Template.create_listing.events({
+    'click .applyByWebsite': function (e, t) {
+      $('.applicationURL').css("display","block");
+    },
+    'click .applyByEmail': function (e, t) {
+      $('.applicationURL').css("display","none");
+    },
+    'submit form':function(e,t){
+      /*if(typeof Session.get('listings') == "undefined")
+      {
+        Session.set('listings')
+      }
+      var listing = {};
+      listing["title"]= e.target.title.value;
+      listing["description"]=e.target.description.value;
+      listing["perks"]=e.target.perks.value;
+      switch (e.target.category.value)
+      {
+        case "one":listing["category"]="Category 1"; break;
+        case "two":listing["category"]="Category 2"; break;
+        case "three":listing["category"]="Category 3"; break;
+        case "four":listing["category"]="Category 4"; break;
+        case "five":listing["category"]="Category 5"; break;
+      }
+      if(e.target.apply.value=="website")
+      {
+        listing["appsite"]= true;
+        listing["website"]= e.target.app_URL.value;
+      }else{
+        listing["appsite"]= false;
+        listing["website"]= "N/A";
+      }*/
+    }
+  });
+}
